6f7846418eeeb8b9e8cd1cd8973410442d936052
Benoit C. Sirois <benoitcsirois@gmail.com>
2013-06-15 19:51:25 -0400

    On peut maintenant choisir les colonnes qui sont affichées

--- seao_ouvert/ui/static/assets/app.js
+++ seao_ouvert/ui/static/assets/app.js
@@ -4,10 +4,50 @@ angular.module('seao', ['ui.directives', 'ui.bootstrap', 'restangular', 'ngGrid'
 	RestangularProvider.setRequestSuffix('/?');
 	RestangularProvider.setListTypeIsArray(false);
     });
-function MainCtrl($scope, Restangular) {
-    $scope.avis = Restangular.all('avis').getList().get('objects');
 
+
+function MainCtrl($scope, Restangular) {
+    $scope.availColumns = [];
+    $scope.selectedColumns = {};
+    $scope.gridFields = [];
     $scope.gridOptions = {
-	data: 'avis'
-    };
+	data: 'avis',
+	columnDefs: 'gridFields'
+    }
+
+    var firstLoad = true;
+    Restangular.all('avis').getList().get('objects').then(function(objs){
+	$scope.avis = objs;
+	console.log('test');
+	if (objs.length) {
+	    for (pr in objs[0]){
+		$scope.availColumns.push(pr);
+		if (firstLoad){
+		    $scope.selectedColumns[pr] = false;
+		    if (pr == 'numero') {
+			$scope.selectedColumns[pr] = true;
+		    }
+		}
+	    }
+	    if (firstLoad){
+		$scope.$broadcast('colChanged', $scope.selectedColumns);
+		firstLoad = false;
+	    }
+
+	}
+    });
+
+    $scope.$on('colChanged', function(evt, selectedCols){
+	console.log('received');
+	var tmpFields = []
+	for (var col in selectedCols) {
+	    if (selectedCols[col]) {
+		tmpFields.push({
+		    field: col,
+		    displayName: col
+		})
+	    }
+	}
+	$scope.gridFields = tmpFields;
+    });
 }
