57707aef2170f23ea0926953cbbe8a6442df1dd1
Louis-Bertrand Varin <louisvarin@gmail.com>
2013-06-15 19:03:17 -0400

    options parsing added, read from file

--- seao_ouvert/api/management/commands/load_avis.py
+++ seao_ouvert/api/management/commands/load_avis.py
@@ -1,14 +1,28 @@
 from django.core.management.base import BaseCommand
-from models import *
+from seao_ouvert.api.models import *
+from optparse import make_option
 
 class Command(BaseCommand):
 
     URL_AVIS = "seao.pourvotre.info/avis.json"
 
-    parser.add_option("-f", "--fichier", action="store",\
-                      type="string", dest="fichier", default=None)
+    option_list = BaseCommand.option_list +  (
+
+        make_option('-f', '--fichier',
+        action='store',
+        dest='fichier',
+        default=None,
+        help='Load les avis depuis un fichier source .json'),
+
+        )
 
     help = BaseCommand.help + "Load les donnees des avis a partir dun fichier json"
 
     def handle(self, *args, **options):
-        pass
+        
+        if options['fichier']:
+            pass
+            #load from file
+        else:
+            pass
+            #load from url
