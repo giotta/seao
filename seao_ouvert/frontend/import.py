351e76aa7b53809a5ef5d20868c9115078335cbc
Davin Baragiotta <davin.baragiotta@auf.org>
2013-06-27 22:01:15 -0400

    import logic : download, extract

--- seao_ouvert/frontend/import.py
+++ seao_ouvert/frontend/import.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
-import json
+import urllib2
+from zipfile import ZipFile
+
 from seao_ouvert.api import models as api
 
 
@@ -8,15 +10,16 @@ SOURCE_URL = "http://http://www.donnees.gouv.qc.ca/?node=/donnees-details&id=542
 SOURCE_DOWNLOAD_URL = "http://donnees.gouv.qc.ca/geonetwork/srv/en/resources.get?id=999&access=private&fname="
 SOURCE_FILES = []
 
-LOCAL_PATH = "data/"
+LOCAL_PATH = "../../data/"
 LOCAL_FILES = []
-LOCAL_DATA_XML = []
-LOCAL_DATA_JSON = []
+
 
 def get_files_from_source():
     print "Finding data files from source :"
 
     # TODO : code logic
+    # pour une date de début...
+    # ... rechercher fichiers égaux ou plus grands que date début
     findings = ['Avis_20130501_20130531.zip',]
     for file in findings:
         SOURCE_FILES.append(file)
@@ -31,42 +34,33 @@ def download_files():
     print "Downloading data files from source :"
 
     for file in SOURCE_FILES:
-        # TODO : code logic
+        source_path = "%s%s" % (SOURCE_DOWNLOAD_URL, file,)
+        local_path = "%s%s" % (LOCAL_PATH, file,)
+
+        zip_file = urllib2.urlopen(source_path)
+        
+        with open(local_path,'wb') as f: f.write(zip_file.read())
+
         LOCAL_FILES.append(file)
+
         print "From :"
-        print "%s%s" % (SOURCE_DOWNLOAD_URL, file,)
+        print source_path
         print "Download to :"
-        print "%s%s" % (LOCAL_PATH, file,)
+        print local_path
 
 def extract_data():
     print "Extracting data from local files :"
 
-    for zip_file in LOCAL_FILES:
-        # TODO : code logic
-        xml_file = zip_file + ".xml"
-        LOCAL_DATA_XML.append(xml_file)
-        print "From :"
-        print "%s%s" % (LOCAL_PATH, zip_file)
-        print "Extracting to :"
-        print "%s%s" % (LOCAL_PATH, xml_file)
+    for file in LOCAL_FILES:
+        zip_file_path = "%s%s" % (LOCAL_PATH, file)
 
-def convert_data_in_json():
-    print "Converting local data from XML to JSON format :"
-    for xml_file in LOCAL_DATA_XML:
-        # TODO : code logic
-        json_file = xml_file + ".json"
-        LOCAL_DATA_JSON.append(json_file)
-        print "From :"
-        print "%s%s" % (LOCAL_PATH, xml_file)
-        print "Converting to :"
-        print "%s%s" % (LOCAL_PATH, json_file)
+        with ZipFile(zip_file_path, 'r') as zip_file:
+            zip_file.extractall(LOCAL_PATH)
 
-def create_objects():
-    print "Creating local objects from local JSON :"
-    # TODO ; code logic
-    for file in LOCAL_DATA_JSON:
-        #avis = json.loads(file)
-        print file
+        print "From :"
+        print zip_file_path
+        print "Extracting all files to :"
+        print "%s" % (LOCAL_PATH,)
 
 if __name__ == "__main__":
     print "SEAO Ouvert"
@@ -78,9 +72,6 @@ if __name__ == "__main__":
     print "--------------------------------"
     extract_data()
     print "--------------------------------"
-    convert_data_in_json()
-    print "--------------------------------"
-    LOCAL_DATA_JSON.append('avis.json')  # TODO : temporary hack
-    create_objects()
+    print "Call load_xml to rock"
     print "================================"
     print "END of local import of data from source"
